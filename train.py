260d15b85f4021f54aed8a0a87d869ae29169978
Akash Singh <akashsingh27101998@gmai.com>
2020-05-08 00:42:58 +0530

    add developer

--- train.py
+++ train.py
@@ -7,6 +7,8 @@ Created on Sun May 3 2020
 
 import tkinter as tk
 from tkinter import Message, messagebox, Text
+import tkinter.ttk as ttk
+import tkinter.font as font
 import cv2
 import os
 import random
@@ -18,18 +20,13 @@ import PIL
 import pandas as pd
 import datetime
 import time
-import tkinter.ttk as ttk
-import tkinter.font as font
 
 window = tk.Tk()
-# helv36 = tk.Font(family='Helvetica', size=36, weight='bold')
+# helv36 = tk.font(family='Helvetica', size=36, weight='bold')
 window.title("Face_Recogniser")
 
-dialog_title = 'QUIT'
-dialog_text = 'Are you sure?'
-# answer = messagebox.askquestion(dialog_title, dialog_text)
  
-#window.geometry('1280x720')
+# window.geometry('1280x720')
 window.configure(background='blue')
 
 window.attributes('-fullscreen', True)
@@ -62,9 +59,48 @@ canvas.create_image(0, 0, image=photo, anchor=tk.NW)
 
 
 
-message = tk.Label(window, text="Face-Recognition-Based-Attendance-Management-System" ,bg="Green"  ,fg="white"  ,width=50  ,height=3,font=('times', 30, 'italic bold underline')) 
 
-message.place(x=200, y=20)
+def tick():
+    time_string = time.strftime('%H:%M:%S')
+    clock.config(text=time_string)
+    clock.after(200,tick)
+ts = time.time()
+date = datetime.datetime.fromtimestamp(ts).strftime('%d-%m-%Y')
+day,month,year=date.split("-")
+
+mont={'01':'January',
+      '02':'February',
+      '03':'March',
+      '04':'April',
+      '05':'May',
+      '06':'June',
+      '07':'July',
+      '08':'August',
+      '09':'September',
+      '10':'October',
+      '11':'November',
+      '12':'December'
+      }
+
+
+frame3 = tk.Frame(window, bg="#c4c6ce")
+frame3.place(relx=0.52, rely=0.09, relwidth=0.09, relheight=0.07)
+
+frame4 = tk.Frame(window, bg="#c4c6ce")
+frame4.place(relx=0.36, rely=0.09, relwidth=0.16, relheight=0.07)
+
+datef = tk.Label(frame4, text = day+"-"+mont[month]+"-"+year+"  | ", fg="orange",bg="#262523",width=55 ,height=1,font=('times', 22, ' bold '))
+datef.pack(fill='both',expand=1)
+
+clock = tk.Label(frame3,fg="orange",bg="#262523" ,width=55 ,height=1,font=('times', 22, ' bold '))
+clock.pack(fill='both',expand=1)
+tick()
+
+message3 = tk.Label(window, text="Face-Recognition-Based-Attendance-Management-System" ,bg="Green"  ,fg="white"  ,width=50  ,height=1,font=('times', 30, 'italic bold underline'))
+message3.place(x=200, y=20)
+
+# message = tk.Label(window, text="Face-Recognition-Based-Attendance-Management-System" ,bg="Green"  ,fg="white"  ,width=50  ,height=3,font=('times', 30, 'italic bold underline'))
+# message.place(x=200, y=20)
 
 lbl = tk.Label(window, text="Enter ID",width=20  ,height=2  ,fg="red"  ,bg="yellow" ,font=('times', 15, ' bold ') ) 
 lbl.place(x=400, y=200)
@@ -84,13 +120,29 @@ lbl3.place(x=400, y=400)
 message = tk.Label(window, text="" ,bg="yellow"  ,fg="red"  ,width=30  ,height=2, activebackground = "yellow" ,font=('times', 15, ' bold ')) 
 message.place(x=700, y=400)
 
-lbl3 = tk.Label(window, text="Attendance : ",width=20  ,fg="red"  ,bg="yellow"  ,height=2 ,font=('times', 15, ' bold  underline')) 
+lbl3 = tk.Label(window, text="Attendance : ",width=20  ,fg="red"  ,bg="yellow"  ,height=2 ,font=('times', 15, ' bold  underline'))
 lbl3.place(x=400, y=650)
 
+# frame1 = tk.Frame(window, bg="#00aeff")
+# frame1.place(relx=0.11, rely=0.17, relwidth=0.39, relheight=0.80)
+# message2 = ttk.Treeview(window,height =5,columns = ('name','date','time'))
+# message2.column('#0',width=82)
+# message2.column('name',width=130)
+# message2.column('date',width=133)
+# message2.column('time',width=133)
+# message2.place(x=700, y=650)
+# # tv.grid(row=2,column=0,padx=(0,0),pady=(150,0),columnspan=4)
+# message2.heading('#0',text ='ID')
+# message2.heading('name',text ='NAME')
+# message2.heading('date',text ='DATE')
+# message2.heading('time',text ='TIME')
 
-message2 = tk.Label(window, text="" ,fg="red"   ,bg="yellow",activeforeground = "green",width=30  ,height=2  ,font=('times', 15, ' bold ')) 
+
+
+
+message2 = tk.Label(window, text="" ,fg="red"   ,bg="yellow",activeforeground = "green",width=30, height=4, font=('times', 15, ' bold '))
 message2.place(x=700, y=650)
- 
+
 def clear():
     txt.delete(0, 'end')    
     res = ""
@@ -243,21 +295,32 @@ def TrackImages():
   
 clearButton = tk.Button(window, text="Clear", command=clear  ,fg="red"  ,bg="yellow"  ,width=20  ,height=2 ,activebackground = "Red" ,font=('times', 15, ' bold '))
 clearButton.place(x=950, y=200)
+
 clearButton2 = tk.Button(window, text="Clear", command=clear2  ,fg="red"  ,bg="yellow"  ,width=20  ,height=2, activebackground = "Red" ,font=('times', 15, ' bold '))
-clearButton2.place(x=950, y=300)    
+clearButton2.place(x=950, y=300)  
+
 takeImg = tk.Button(window, text="Take Images", command=TakeImages  ,fg="red"  ,bg="yellow"  ,width=20  ,height=3, activebackground = "Red" ,font=('times', 15, ' bold '))
 takeImg.place(x=200, y=500)
+
 trainImg = tk.Button(window, text="Train Images", command=TrainImages  ,fg="red"  ,bg="yellow"  ,width=20  ,height=3, activebackground = "Red" ,font=('times', 15, ' bold '))
 trainImg.place(x=500, y=500)
+
 trackImg = tk.Button(window, text="Track Images", command=TrackImages  ,fg="red"  ,bg="yellow"  ,width=20  ,height=3, activebackground = "Red" ,font=('times', 15, ' bold '))
 trackImg.place(x=800, y=500)
+
 quitWindow = tk.Button(window, text="Quit", command=window.destroy  ,fg="red"  ,bg="yellow"  ,width=20  ,height=3, activebackground = "Red" ,font=('times', 15, ' bold '))
 quitWindow.place(x=1100, y=500)
-copyWrite = tk.Text(window, background=window.cget("background"), borderwidth=0,font=('times', 30, 'italic bold underline'))
-copyWrite.tag_configure("superscript", offset=10)
-copyWrite.insert("insert", "Developed by Akash Singh","", "", "")
-copyWrite.configure(state="disabled",fg="red"  )
-copyWrite.pack(side="left")
-copyWrite.place(x=800, y=750)
+
+def function5():    
+   os.startfile(os.getcwd()+"/developers/diet1frame1first.html");
+about = tk.Button(window,text="Developer",font=('times new roman',40),bg="green",fg="red",activebackground = "Red", command=function5)
+about.place(x=1100, y=750)
+
+copyWrite = tk.Text(window, background=window.cget("background"), borderwidth=0,font=('times', 50, 'italic bold underline'))
+copyWrite.tag_configure("superscript", offset=20)
+# copyWrite.insert("insert", "Developed by Akash Singh","", "", "")
+# copyWrite.configure(state="disabled",fg="red"  )
+# copyWrite.pack(side="left")
+# copyWrite.place(x=800, y=750)
  
 window.mainloop()
